f5857424cc49f9b35963957af97031dc975d4129
l-erw <lizzieerwood@gmail.com>
2022-03-02 19:59:15 +0000

    added toggle between rgb and hex codes

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-const getColourBtn = document.getElementById("get-colour")
+const chosenColour = document.getElementById("colour-picker")
+const chosenScheme = document.getElementById("scheme-selector")
+const chosenColourCode = document.getElementById("chosen-colour-code")
 
+chosenColour.oninput = () => {
+    getColour()
+}
 
+chosenScheme.oninput = () => {
+    getColour()
+}
+
+chosenColourCode.oninput = () => {
+    getColour()
+} 
 
 function getColour() {
-    const chosenColour = document.getElementById("colour-picker")
-    const chosenScheme = document.getElementById("scheme-selector")
+    const colourContainer = document.querySelector(`.colours-display`)
+    const colourValueContainer = document.querySelector(".colour-names")
     scheme = chosenScheme.value
     colour = chosenColour.value.substring(1)
-    const chosenColourCode = document.getElementById("chosen-colour-code")
+    // code = chosenColour.value
     chosenColourCode.textContent = `#${colour}`
     fetch(`https://www.thecolorapi.com/scheme?hex=${colour}&mode=${scheme}&count=5`)
     .then(res => res.json())
     .then(data => {
-        const colourContainer = document.querySelector(`.colours-display`)
-        const colourValueContainer = document.querySelector(".colour-names")
         colourContainer.innerHTML = ""
         colourValueContainer.innerHTML = ""
         for(let i = 0; i < 5; i++) {
-                const colorHex = data.colors[i].hex.value;
-                colourContainer.innerHTML += `<div class="colour" style="background-color: ${colorHex}"></div>`
-                colourValueContainer.innerHTML += `<div class="colour-id">${colorHex}</div>`
+                if (changeColourCode.checked) {
+                    const colorRgb = data.colors[i].rgb.value;
+                    colourContainer.innerHTML += `<div class="colour" style="background-color: ${colorRgb}"></div>`
+                    colourValueContainer.innerHTML += `<div class="colour-id">${colorRgb.slice(4, -1)}</div>`
+                    chosenColourCode.value = colorRgb.slice(4, -1) 
+                  } else {
+                    const colorHex = data.colors[i].hex.value;
+                    colourContainer.innerHTML += `<div class="colour" style="background-color: ${colorHex}"></div>`
+                    colourValueContainer.innerHTML += `<div class="colour-id">${colorHex}</div>`
+                    chosenColourCode.value = colorHex
+                  }
             }     
     })
 }
 
-
-getColourBtn.addEventListener("click", getColour)
+const changeColourCode = document.getElementById("toggle")
+changeColourCode.addEventListener("click", getColour)
